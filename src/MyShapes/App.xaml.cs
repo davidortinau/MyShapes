b0e6266e10c69f14c91fa19db46e7db13138604b
David Ortinau <david.ortinau@microsoft.com>
2020-07-15 16:43:13 -0500

    code and screenshots

---
+++ src/MyShapes/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyShapes
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            StyleSheetRegistrar.RegisterStyle("-xf-horizontal-options", typeof(VisualElement), nameof(View.HorizontalOptionsProperty));
+            StyleSheetRegistrar.RegisterStyle("-xf-vertical-options", typeof(VisualElement), nameof(View.VerticalOptionsProperty));
+
+            Device.SetFlags(new string[] { "Shapes_Experimental" });
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
